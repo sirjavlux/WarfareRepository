689073cb4a035a020b88327f480f0f45d7e93ab0
PluginBuild <sirjavlux12345@gmail.com>
2020-04-14 11:47:46 +0200

    Body part hit registration 0.1 completed

--- WarfarePlugin/src/com/coding/sirjavlux/core/DamageListener.java
+++ WarfarePlugin/src/com/coding/sirjavlux/core/DamageListener.java
@@ -1,6 +1,7 @@
 package com.coding.sirjavlux.core;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
@@ -17,43 +18,51 @@ public class DamageListener implements Listener {
 	public void playerDamagePlayerEvent(EntityDamageByEntityEvent e) {
 		if (e.getDamager() instanceof Projectile) {
 			Projectile ptile = (Projectile) e.getDamager();
-			String[] data = ptile.getCustomName().split(",");
-			if (data[0].equalsIgnoreCase("projectile")) {
-				Vector pDir = ptile.getLocation().getDirection();
-				LivingEntity damaged = (LivingEntity) e.getEntity();
-				double damage = Double.parseDouble(data[1]);
-				
-				BodyPart part = damaged instanceof Player ? getHitBodyPart(ptile.getLocation(), damaged.getLocation(), pDir) : BodyPart.Chest;
-				//temporary
-				getHitBodyPart(ptile.getLocation(), damaged.getLocation(), pDir);
-				
-				System.out.println("damage " + damage * Double.parseDouble(part.toString()));
-				e.setDamage(damage * Double.parseDouble(part.toString()));
+			if (ptile.getCustomName() != null) {
+				String[] data = ptile.getCustomName().split(",");
+				if (data[0].equalsIgnoreCase("projectile")) {
+					Vector pDir = ptile.getLocation().getDirection();
+					LivingEntity damaged = (LivingEntity) e.getEntity();
+					double damage = Double.parseDouble(data[1]);
+					
+					BodyPart part = damaged instanceof Player ? getHitBodyPart(ptile.getLocation(), damaged.getLocation(), pDir) : BodyPart.Chest;
+					
+					getHitBodyPart(ptile.getLocation(), damaged.getLocation(), pDir); //temporary
+					
+					System.out.println("damage " + damage * Double.parseDouble(part.toString()));
+					e.setDamage(damage * Double.parseDouble(part.toString()));
+				}
 			}
 		}
 	}
 	
-	private BodyPart getHitBodyPart(Location hitLoc, Location pLoc, Vector pDir) {
+	///////////////////////////////////
+	//body part registration 0.1
+	///////////////////////////////////
+	private BodyPart getHitBodyPart(Location hitLoc, Location pLoc, Vector oldDir) {
 		BodyPart hitBodyPart = BodyPart.Chest;
 		
+		double oldDirY = oldDir.getY();
 		Location l1 = pLoc.clone();
 		Location l2 = hitLoc.clone();
 		l1.setY(0);
 		l2.setY(0);
+
+		//create projectile direction
+		Vector pDir = l1.toVector().subtract(l2.toVector());
+		pDir.setX(pDir.getX() / (l1.distance(l2) + l1.distance(l2)));
+		pDir.setZ(pDir.getZ() / (l1.distance(l2) + l1.distance(l2)));
+		double distance = l1.distance(l2) + l1.distance(l2) * ((1 - pDir.lengthSquared()) > 0 ? (1 - pDir.lengthSquared()) : (1 - pDir.lengthSquared()) * -1);
+		pDir.setY(oldDirY);
 		
-		//fix distance issue <-----
-		System.out.println(l1.distance(l2));
-		
-		hitLoc = hitLoc.toVector().add(pDir.multiply(l1.distance(l2))).toLocation(pLoc.getWorld());
+		hitLoc = hitLoc.toVector().subtract(pDir.multiply(distance)).toLocation(pLoc.getWorld());
 		l2 = hitLoc.clone();
 		l2.setY(0);
 		
-		System.out.println(l1.distance(l2));
-		
 		//above waist
-		if (hitLoc.getY() - pLoc.getY() > 0.85) {
+		if (hitLoc.getY() - pLoc.getY() > 0.8) {
 			//check if headshot
-			if (hitLoc.getY() - pLoc.getY() > 1.45) {
+			if (hitLoc.getY() - pLoc.getY() > 1.5) {
 				System.out.println("headshot");
 				hitBodyPart = BodyPart.Head;
 			} 
